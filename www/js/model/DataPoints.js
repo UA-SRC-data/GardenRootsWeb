8e9c27e098d1cd1f1cf58f404697d1b654e6a261
jiachengyang <kv9699@gmail.com>
2020-02-19 13:53:52 -0700

    implemented necessary functions for drawing data points

--- www/js/model/DataPoints.js
+++ www/js/model/DataPoints.js
@@ -5,11 +5,18 @@ class DataPoints{
     colorScale;
     sizeScale;
 
-
-
     constructor(dataSet, mineral, colorScale, sizeScale){
+        this.dataSet = dataSet;
+        this.mineral = mineral;
+        this.colorScale = colorScale;
+        this.sizeScale = sizeScale;
     }
 
+    calculateSize(value){
+        return this.sizeScale(value);
+    }
 
-
+    calculateColor(value){
+        return this.colorScale(value);
+    }
 }
--- www/js/model/DataSet.js
+++ www/js/model/DataSet.js
@@ -2,11 +2,14 @@ class DataSet {
 
     name;
     jsonData;
+    availableMinerals;
     callBack;
+    dataPoints = {};
+    maxValues = {};
 
-    constructor(setName, setPath, callBack) {
+    constructor(setName, setPath, availableMinerals) {
         this.name = setName;
-        this.callBack = callBack;
+        this.availableMinerals = availableMinerals;
         d3.json(setPath, (err, data) => {
             if (err) {// todo err message
                 console.log(err);
@@ -15,9 +18,40 @@ class DataSet {
         })
     }
 
-    setUp() {
-        this.callBack(this.jsonData)
+    #getMaxValues(mineral){
+        //todo implement
+        return 1000000000
     }
 
+    #getColorScale(mineral){
+        if (this.availableMinerals.hasOwnProperty(mineral)){
+            let colorScale =  d3.scaleLinear().domain([0, this.availableMinerals[mineral]]).range([Model.colors]);
+            return (value)=>{
+                if (value> this.availableMinerals[mineral]){
+                    return Model.maxColor;
+                }else{
+                    return colorScale(value);
+                }
+            };
+        }
+        return d3.scaleLinear().domain([0, this.#getMaxValues(mineral)]).range(["white", "purple"]);
+    }
 
+    getDataPointObj(mineral){
+        if (!this.dataPoints.hasOwnProperty(mineral)){
+            this.dataPoints[mineral] =
+                new DataPoints(this.name, mineral, this.#getColorScale(mineral),
+                    d3.scaleLinear().domain([1,5]).range([1,5])) //because we are just using the number of point.
+        }
+    }
+
+    calculateSize(mineral, value){
+        if(! this.dataPoints.hasOwnProperty(mineral)){}//todo throw error
+        return this.dataPoints[mineral].calculateSize(value);
+    }
+
+    calculateColor(mineral, value){
+        if(! this.dataPoints.hasOwnProperty(mineral)){}//todo throw error
+        return this.dataPoints[mineral].calculateColor(value);
+    }
 }
