6633d44559ce220b22bb83549b77624090a9c710
jiachengyang <kv9699@gmail.com>
2020-02-20 17:02:30 -0700

    added code for drawing data points in controller

--- www/js/controller/Controller.js
+++ www/js/controller/Controller.js
@@ -3,18 +3,28 @@ class Controller{
     view;
     model;
     backgroundMap = undefined;
+    currentDataSet;
+    currentDataPoint;
 
     constructor(view, model){
         this.model = model;
         this.view = view;
+        this.currentDataSet = undefined;
+        this.currentDataPoint = undefined;
     }
 
-    setUPBackGroundMap(callBack){
+    setUpBackGroundMap(callBack){
         if (this.backgroundMap !== undefined){
             return; // todo maybe generate an error.
         }
         this.backgroundMap = this.model.getBackGroundMapObj(callBack);
         this.backgroundMap.setUp();
+    }
+
+    setUpPoints(callBack){
+        this.currentDataSet = this.model.getDataSetObj();
+        this.currentDataPoint = this.model.getDataPointObj();
+        this.currentDataSet.setUpPoints(callBack);
     }
 
     setCurrentDataSet(dataSetsName){
@@ -24,4 +34,22 @@ class Controller{
     setCurrentMineral(mineralName){
         this.model.setCurrentMineral(mineralName);
     }
+
+    calculateSize(value){
+        if (this.currentDataSet === undefined || this.currentDataPoint === undefined){
+            // todo throw error
+        }
+        this.currentDataSet.calculateSize(value);
+    }
+
+
+    calculateColor(value){
+        if (this.currentDataSet === undefined || this.currentDataPoint === undefined){
+            // todo throw error
+        }
+        this.currentDataSet.calculateColor(value);
+    }
+
+
+
 }
