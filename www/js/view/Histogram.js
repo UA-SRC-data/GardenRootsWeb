3504bcfa2bac8a5a662a50309039abcf4a5cced8
jiachengyang <kv9699@gmail.com>
2020-02-26 14:02:30 -0700

    ready to format code

--- www/js/view/Histogram.js
+++ www/js/view/Histogram.js
@@ -1,10 +1,11 @@
 class Histogram {
+    static numberOfStep = 6;
+
     layer;
     controller;
     dataPointPrompt;
     xOffset = 50;
     step = 50;
-    yOffset = 5;
 
     constructor(svg, controller) {
         this.layer = svg.append("g");
@@ -13,30 +14,27 @@ class Histogram {
     }
 
     callbackDrawHistogram = (points) => {
-        let data = [];
-        for (let i = 0; i < points.length; i++) {
-            data.push(this.controller.getSampleAverage(points[i]));
-        }
-        data.sort((a, b) => {
-            return a - b;
-        });
+        // get the average of each point.
+        let data = points.map((v) => this.controller.getSampleAverage(v));
+        data.sort((a, b) => a - b);
+
         let max = Math.max(...data);
         let groupedData = [0, 0, 0, 0, 0, 0];
-        let indexScale = d3.scaleQuantize().domain([0, max]).range([0, 1, 2, 3, 4, 5]);
+        let value2IndexScale = d3.scaleQuantize().domain([0, max]).range([0, 1, 2, 3, 4, 5]);
         for (let i = 0; i < data.length; i++) {
-            groupedData[indexScale(data[i])]++;
+            groupedData[value2IndexScale(data[i])]++;
         }
-        let heightScale = d3.scaleLinear().domain([0, Math.max(...groupedData)]).range([0, 400]);
+        let number2HeightScale = d3.scaleLinear().domain([0, Math.max(...groupedData)]).range([0, 400]);
         //let xScale = heightScale.reverse;
         let yScale = d3.scaleLinear().domain([0, Math.max(...groupedData)]).range([500, 100]);
-        let xScale = d3.scaleLinear()
+        let xScale = d3.scaleLinear() //value2position
             .domain([0,
-                indexScale.invertExtent(0)[1],
-                indexScale.invertExtent(1)[1],
-                indexScale.invertExtent(2)[1],
-                indexScale.invertExtent(3)[1],
-                indexScale.invertExtent(4)[1],
-                indexScale.invertExtent(5)[1]])
+                value2IndexScale.invertExtent(0)[1],
+                value2IndexScale.invertExtent(1)[1],
+                value2IndexScale.invertExtent(2)[1],
+                value2IndexScale.invertExtent(3)[1],
+                value2IndexScale.invertExtent(4)[1],
+                value2IndexScale.invertExtent(5)[1]])
             .range([this.xOffset,
                 this.step + this.xOffset,
                 this.step * 2 + this.xOffset,
@@ -60,15 +58,15 @@ class Histogram {
                 return this.step;
             })
             .attr("height", (d, i) => {
-                return heightScale(d);
+                return number2HeightScale(d);
             })
             .attr("stroke", "black")
             .attr("stroke-width", "2px")
             .attr("fill", (d, i) => {
-                return this.controller.calculateColor(indexScale.invertExtent(i)[1]);
+                return this.controller.calculateColor(value2IndexScale.invertExtent(i)[1]);
             });
         this.layer.append("g").attr("transform", "translate(40, 0)").call(d3.axisLeft(yScale));
-        this.layer.append("g").attr("transform","translate(0, 510)").call(d3.axisBottom(xScale).tickValues(xScale.domain()));
+        this.layer.append("g").attr("transform", "translate(0, 510)").call(d3.axisBottom(xScale).tickValues(xScale.domain()));
     };
 
     erase = () => {
