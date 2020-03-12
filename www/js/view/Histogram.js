a6615aaa161139388d003095bfbecc60a0dd7153
kv9699 <kv9699@gmail.com>
2020-03-12 15:35:24 -0700

    fixed a typo in jsdoc

--- www/js/view/Histogram.js
+++ www/js/view/Histogram.js
@@ -24,7 +24,7 @@ class Histogram {
     /** @type {Number} */
     static step = 50;
 
-    /**@member {D3Selection} layer - a instance of controller - the svg dom for background map*/
+    /**@member {D3Selection} layer - the svg dom for background map*/
     layer;
     /** @member {Controller} controller - the controller */
     controller;
