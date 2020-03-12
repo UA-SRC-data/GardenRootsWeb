fc7a317d4b7121911a8a5a163d8517a070ac6817
kv9699 <kv9699@gmail.com>
2020-03-12 01:36:46 -0700

    renamed

--- www/js/view/Points.js
+++ www/js/view/Points.js
@@ -1,14 +1,29 @@
+/**
+ * This class depicts the circles (points) on the map
+ */
 class Points {
+    /** @member {D3Selection} layer - the svg group for circles */
     layer;
+    /** @member {Controller} controller - the controller */
     controller;
+    /** @member {DataPointPrompt} dataPointPrompt - a instance of DataPointPrompt */
     dataPointPrompt;
 
+    /**
+     * This is the constructor
+     * @param {D3Selection} svg - the main svg dom
+     * @param {Controller} controller - a instance of controller object
+     */
     constructor(svg, controller) {
         this.layer = svg.append("g");
         this.controller = controller;
         this.dataPointPrompt = new DataPointPrompt(this.layer, controller);
     }
 
+    /**
+     * This is a callback function that draws circles (Points) on the map
+     * @param points
+     */
     callbackDrawPoints = (points) => {
         this.layer
             .selectAll("circle")
@@ -30,6 +45,28 @@ class Points {
         this.dataPointPrompt.bound();
     };
 
+    /**
+     * This callback type is called `circleFilter`
+     *
+     * @callback circleFilter
+     * @param {JSON} value
+     * @return {Boolean}
+     */
+
+    /**
+     * This callback type is called `updatePoints`
+     *
+     * @callback updatePoints
+     * @param {D3Selection} value
+     */
+
+    /**
+     * This is a callback function that is called when user selects a range of data in histogram
+     * @see Histogram
+     *
+     * @param {circleFilter} filter
+     * @param {updatePoints} callBack
+     */
     update = (filter, callBack) => {
         let filteredPoints = this.layer
             .selectAll("circle")
@@ -37,10 +74,16 @@ class Points {
         callBack(filteredPoints);
     };
 
+    /**
+     * This function is called when user zoom in or out.
+     */
     zoom = () => {
         this.layer.attr("transform", d3.event.transform);
     };
 
+    /**
+     * This function erase all circles (points)
+     */
     erase = () => {
         this.layer.selectAll(".points").remove();
         this.layer.selectAll(".points")
