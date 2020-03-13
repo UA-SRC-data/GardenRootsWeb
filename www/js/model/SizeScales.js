5577aa0e9d338198575eb831c0a37595b61eb2a8
kv9699 <kv9699@gmail.com>
2020-03-12 22:25:30 -0700

    more jsdoc

--- www/js/model/SizeScales.js
+++ www/js/model/SizeScales.js
@@ -1,12 +1,28 @@
+/**
+ * This class manages size scales.
+ */
 class SizeScales{
+    /** @type {number[]}*/
     static defaultDomain = [1, 5];
+    /** @type {number[]}*/
     static defaultRange = [1, 5];
 
+    /** @member {{string: function}} scales */
     scales = {};
 
+    /**
+     * This is the constructor.
+     */
     constructor() {
     }
 
+    /**
+     * Call this function to calculate the size of circle of given contaminant and value
+     *
+     * @param {string} contaminant
+     * @param {number} value
+     * @return {number}
+     */
     calculateSize(contaminant, value) {
         if (!this.scales.hasOwnProperty(contaminant)){
             this.scales[contaminant] = d3.scaleLinear().domain(SizeScales.defaultDomain).range(SizeScales.defaultRange);
