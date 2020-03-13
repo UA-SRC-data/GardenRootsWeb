58f991c06be7fc98b6bd380f082de7579dcc4430
kv9699 <kv9699@gmail.com>
2020-03-12 22:17:42 -0700

    more jsdoc

--- www/js/model/BackGroundMap.js
+++ www/js/model/BackGroundMap.js
@@ -1,12 +1,25 @@
+/**
+ * This class stores the geo json of arizona map
+ */
 class BackGroundMap{
 
+    /** @member {string} */
     path;
+    /** @member {JSON} */
     jsonData;
 
+    /**
+     * This is the constructor
+     * @param path
+     */
     constructor(path) {
         this.path = path;
     }
 
+    /**
+     * Call this function to get the geojson data
+     * @param {setUpBackGroundMapCallback} callback
+     */
     setUp(callback){
         if (this.jsonData === undefined) {
             d3.json(this.path).then((data) => {
