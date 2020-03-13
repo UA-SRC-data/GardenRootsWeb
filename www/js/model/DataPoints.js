c86c0c4ed49d7baa1aebfbed6468e5fb220a2b8e
kv9699 <kv9699@gmail.com>
2020-03-12 20:19:54 -0700

    restructured DataPoints.js

--- www/js/model/DataPoints.js
+++ www/js/model/DataPoints.js
@@ -1,28 +1,88 @@
+/**
+ * @typedef {{coordinates: Number[], samples: {String: Number[]}}} dataPoint
+ */
+
+/**
+ * @typedef {{coordinates: Number[],
+ *            samples: Number[],
+ *            numPoints: Number,
+ *            average: Number,
+ *            median: Number,
+ *            exceed: Number,
+ *            max: Number,
+ *            color: String,
+ *            size: Number
+ *            }} dataPointWithAssociatedInfo
+ */
+
+
+/**
+ * This class represents a circle in the map
+ */
 class DataPoints{
 
-    dataSet;
-    contaminant;
     colorScale;
     sizeScale;
-    LegendSample = [20, 10, 5, 2, 1];
+    /** @member {dataPoint} */
+    data;
+    LegendSample = [20, 10, 5, 2, 1];// todo move to data set layer
 
-    constructor(dataSet, contaminant, colorScale, sizeScale){
-        this.dataSet = dataSet;
-        this.contaminant = contaminant;
+    constructor(rawData, colorScale, sizeScale){
+        this.data = this.processData(rawData);
         this.colorScale = colorScale;
         this.sizeScale = sizeScale;
     }
 
-    calculateSize(value){
-        return this.sizeScale(value.properties.numPoints);
+    processData(rawData){
+        return {
+            coordinates: rawData.coordinates,
+            samples: rawData.properties.values
+        }
     }
 
-    calculateColor(value){// I call it value because it can be a number or object
-        if (typeof value === "number") {
-            return this.colorScale(value);
-        }else {
-            return this.colorScale(value.properties.averages[this.contaminant]);
+    /**
+     *
+     * @param contaminant
+     * @param callback
+     */
+    getData(contaminant, callback){
+        let contaminantSamples = this.data.samples[contaminant];
+        let average = contaminantSamples.reduce((a, b) => a + b, 0) / contaminantSamples.length;
+        /** @type dataPointWithAssociatedInfo*/
+        let data = {
+            coordinates: this.data.coordinates,
+            samples: contaminantSamples,
+            numPoints: this.data.samples[contaminant].length,
+            average: contaminantSamples.reduce((a, b) => a + b, 0) / contaminantSamples.length,
+            median: this.getMedian(contaminantSamples),
+            exceed: this.getExceed(contaminant, contaminantSamples),
+            max: Math.max(...contaminantSamples),
+            color: this.colorScale(average),
+            size: this.sizeScale( this.data.samples[contaminant].length)
+        };
+        callback(data);
+    }
+
+    getMedian(arr){
+        arr.sort((a, b) => a - b);
+        let lowMiddle = Math.floor((arr.length - 1) / 2);
+        let highMiddle = Math.ceil((arr.length - 1) / 2);
+        return (arr[lowMiddle] + arr[highMiddle]) / 2;
+    }
+
+    getExceed(contaminant, arr){
+        let exceeds = 0;
+        for (let i = 0; i < arr.length; i++){
+            if (arr[i] >= Model.SRLS[contaminant]){  // todo why just use SRLS?????????????????????????????
+                exceeds++;
+            }
         }
+        return exceeds/(arr.length)*100;
+    }
+
+
+    calculateColor(value){// I call it value because it can be a number or object
+        return this.colorScale(value);
     }
 
     getLegendPoints(){
@@ -34,36 +94,5 @@ class DataPoints{
             })
         }
         return data;
-    }
-
-    getNumberOfSamplePoint(value){// for consistency, I just call it value.
-        return value.properties.numPoints;
-    }
-
-    getAllSampleData(value){
-        return value.properties.values[this.contaminant];
-    }
-
-    getSampleAverage(value){
-        return value.properties.averages[this.contaminant];
-    }
-
-    getSampleMedian(value){
-        let values = this.getAllSampleData(value);
-        values.sort((a, b) => a - b);
-        let lowMiddle = Math.floor((values.length - 1) / 2);
-        let highMiddle = Math.ceil((values.length - 1) / 2);
-        return (values[lowMiddle] + values[highMiddle]) / 2;
-    }
-
-    getSampleExceed(value){
-        let values = this.getAllSampleData(value);
-        let exceeds = 0;
-        for (let i = 0; i < values.length; i++){
-            if (values[i] >= Model.SRLS[this.contaminant]){  // todo why just use SRLS?????????????????????????????
-                exceeds++;
-            }
-        }
-        return exceeds/(values.length)*100;
     }
 }
