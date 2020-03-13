3fc1da43dc5cc67a20702b70e59c4a49328e092d
kv9699 <kv9699@gmail.com>
2020-03-12 22:23:26 -0700

    more jsdoc

--- www/js/model/ColorScales.js
+++ www/js/model/ColorScales.js
@@ -1,18 +1,39 @@
+/**
+ * This class manages color scales.
+ */
 class ColorScales{
+    /**@type {string[]}*/
     static allColors = ['#ffffcc', '#c7e9b4', '#7fcdbb', '#41b6c4', '#0c2c84'];
 
+    /** @type {[string, string, string, string]} */
     static colors = Model.colors;
+    /** @type {string} */
     static maxColor = Model.maxColor;
+    /** @type {[string, string]} */
     static defaultColor = Model.defaultColor;
 
 
+    /** @member {{string: number}} refValues */
     refValues;
+    /** @member {{string: function}} scales */
     scales = {};
 
+    /**
+     * This is the constructor.
+     *
+     * @param {{string: number}} refValues
+     */
     constructor(refValues) {
         this.refValues = refValues;
     }
 
+    /**
+     * Call this function to calculate the color of given contaminant and value
+     *
+     * @param {String} contaminant
+     * @param {number} value
+     * @return {string}
+     */
     calculateColor(contaminant, value) {
         if (!this.scales.hasOwnProperty(contaminant)){
             this.scales[contaminant] = this.generateColorScale(contaminant);
@@ -20,6 +41,12 @@ class ColorScales{
         return this.scales[contaminant](value);
     }
 
+    /**
+     * This function has the rule to create a color scale for given contaminant
+     *
+     * @param {String} contaminant
+     * @return {function}
+     */
     generateColorScale(contaminant) {
         if (this.refValues.hasOwnProperty(contaminant)) {
             let colorScale = d3.scaleLinear().domain([0, this.refValues[contaminant]]).range(ColorScales.colors);
